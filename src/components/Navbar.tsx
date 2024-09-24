34f6a264f8ba39b6ea7a28a3beff13e596f8aa9e
humonnom <juepark42@gmail.com>
2024-09-24 18:55:25 +0900

    desktop 구현

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -88,8 +88,22 @@ export const Navbar = () => {
             </>
           )}
         </Disclosure>
+
         {/* 데스크탑 메뉴 */}
-        {/* 데스크탑 메뉴 구현 */}
+        <div className="hidden text-center lg:flex lg:items-center">
+          <ul className="items-center justify-end flex-1 pt-6 list-none lg:pt-0 lg:flex">
+            {navigation.map((menu, index) => (
+              <li className="mr-3 nav__item" key={index}>
+                <Link
+                  href="/"
+                  className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none"
+                >
+                  {menu}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </nav>
     </div>
   );
