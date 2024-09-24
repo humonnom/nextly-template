2ad1489ae9dcee5bfc98faaeeea785ef695e20f6
humonnom <juepark42@gmail.com>
2024-09-24 18:39:10 +0900

    init project

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,31 +5,31 @@ import heroImg from "../../public/img/hero.png";
 export const Hero = () => {
   return (
     <>
-      <Container className="flex flex-wrap ">
-        <div className="flex items-center w-full lg:w-1/2">
-          <div className="max-w-2xl mb-8">
-            <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
+      <Container className="flex flex-wrap">
+        <div className="">
+          <div className="">
+            <h1 className="">
               Free Landing Page Template for startups
             </h1>
-            <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
+            <p className="">
               Nextly is a free landing page & marketing website
               template for startups and indie projects. Its built with
               Next.js & TailwindCSS. And its completely open-source.
             </p>
 
-            <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
+            <div className="">
               <a
                 href="https://web3templates.com/templates/nextly-landing-page-template-for-startups"
                 target="_blank"
                 rel="noopener"
-                className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md ">
+                className="">
                 Download for Free
               </a>
               <a
                 href="https://github.com/web3templates/nextly-template/"
                 target="_blank"
                 rel="noopener"
-                className="flex items-center space-x-2 text-gray-500 dark:text-gray-400">
+                className="">
                 <svg
                   role="img"
                   width="24"
@@ -46,7 +46,7 @@ export const Hero = () => {
             </div>
           </div>
         </div>
-        <div className="flex items-center justify-center w-full lg:w-1/2">
+        <div className="">
           <div className="">
             <Image
               src={heroImg}
@@ -61,28 +61,18 @@ export const Hero = () => {
         </div>
       </Container>
       <Container>
-        <div className="flex flex-col justify-center">
-          <div className="text-xl text-center text-gray-700 dark:text-white">
-            Trusted by <span className="text-indigo-600">2000+</span>{" "}
+        <div className="">
+          <div className="">
+            Trusted by <span className="">2000+</span>{" "}
             customers worldwide
           </div>
 
-          <div className="flex flex-wrap justify-center gap-5 mt-10 md:justify-around">
-            <div className="pt-2 text-gray-400 dark:text-gray-400">
+          <div className="">
               <AmazonLogo />
-            </div>
-            <div className="text-gray-400 dark:text-gray-400">
               <VerizonLogo />
-            </div>
-            <div className="text-gray-400 dark:text-gray-400">
               <MicrosoftLogo />
-            </div>
-            <div className="pt-1 text-gray-400 dark:text-gray-400">
               <NetflixLogo />
-            </div>
-            <div className="pt-2 text-gray-400 dark:text-gray-400">
               <SonyLogo />
-            </div>
           </div>
         </div>
       </Container>
